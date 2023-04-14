76b8c70f226197592ba36cf8e8efdb7d4d37faea
Mario Cardoso Henriques <maricard@c1r14s2.42porto.com>
2023-04-14 12:51:20 +0100

    push swap

--- push_swap/src/stack/check_arguments.c
+++ push_swap/src/stack/check_arguments.c
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 11:03:17 by maricard          #+#    #+#             */
-/*   Updated: 2023/04/13 12:58:29 by maricard         ###   ########.fr       */
+/*   Updated: 2023/04/14 12:44:35 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,21 @@ void	put_to_stack(char **argv, t_root *root)
 	}
 }
 
+int	check_int_range(char **argv, int i)
+{
+	if (argv[i][0] == '-')
+	{
+		if (long_atoi(argv[i]) < INT_MIN)
+			return (1);
+	}
+	else
+	{
+		if (long_atoi(argv[i]) > INT_MAX)
+			return (1);
+	}
+	return (0);
+}
+
 void	is_number(char **argv, t_root *root)
 {
 	int	i;
@@ -70,8 +85,10 @@ void	is_number(char **argv, t_root *root)
 			if (argv[i][a] >= '0' && argv[i][a] <= '9')
 				a++;
 			else
-				ft_error("Arguments must be numbers.", root);
+				ft_error("Invalid Arguments.", root);
 		}
+		if (check_int_range(argv, i) == 1)
+			ft_error("Argument is out of the int range.", root);
 		i++;
 	}
 	root->size = i - 1;
--- push_swap/src/utils/long_atoi.c
+++ push_swap/src/utils/long_atoi.c
@@ -1,43 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sorting.c                                          :+:      :+:    :+:   */
+/*   long_atoi.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/13 15:03:13 by maricard          #+#    #+#             */
-/*   Updated: 2023/04/14 11:07:57 by maricard         ###   ########.fr       */
+/*   Created: 2023/04/14 12:35:10 by maricard          #+#    #+#             */
+/*   Updated: 2023/04/14 12:49:34 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_sorted(t_root *root)
+long long	long_atoi(char *str)
 {
+	long long num;
 	int	i;
+	int	signal;
 
 	i = 0;
-	while (i != root->size - 1)
+	signal = 1;
+	num = 0;
+	if (str[0] == '-')
 	{
-		if (root->stack_a[i] > root->stack_a[i + 1])
-			return (1);
+		signal = signal * -1;
 		i++;
 	}
-	return (0);
-}
-
-void	start_sorting(t_root *root, int argc)
-{
-	if (is_sorted(root) == 0)
-		return ;
-	else if (argc == 3)
-		two(root);
-	else if (argc == 4)
-		three(root);
-	else if (argc >= 5 && argc <= 6)
-		five (root);
-	else if (argc >= 7 && argc <= 101)
-		one_hundred(root);
-	// else if (argc >= 102)
-	// 	five_hundred(root);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	num = num * signal;
+	return (num);
 }
