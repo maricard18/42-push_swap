45db98def936ddb376ca7cedb10c1749c0d8e0ce
Mario Cardoso Henriques <maricard@c1r11s3.42porto.com>
2023-04-20 11:08:45 +0100

    push swap

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 10:33:22 by maricard          #+#    #+#             */
-/*   Updated: 2023/04/20 10:38:59 by maricard         ###   ########.fr       */
+/*   Updated: 2023/04/20 11:06:33 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,10 @@ typedef struct s_root
 }		t_root;
 
 // Functions
-void	check_arguments(char **argv, t_root *root);
-void	start_sorting(t_root *root, int argc);
-void	find_min_a(t_root *root);
-void	find_max_a(t_root *root);
+void		check_arguments(char **argv, t_root *root);
+void		start_sorting(t_root *root, int argc);
+void		find_min_a(t_root *root);
+void		find_max_a(t_root *root);
 
 // Utils
 void		ft_error(t_root *root);
@@ -62,31 +62,31 @@ void		indexing_array(t_root *root);
 long long	long_atoi(char *str);
 
 // Algorithm
-void	two(t_root *root);
-void	three(t_root *root);
-void	four(t_root *root);
-void	five(t_root *root);
- void	radix(t_root *root);
+void		two(t_root *root);
+void		three(t_root *root);
+void		four(t_root *root);
+void		five(t_root *root);
+void		radix(t_root *root);
 // void	five_hundred(t_root *root);
 
 // Operations
 // Swap
-void	sa(t_root *root, bool flag);
-void	sb(t_root *root, bool flag);
-void	ss(t_root *root);
+void		sa(t_root *root, bool flag);
+void		sb(t_root *root, bool flag);
+void		ss(t_root *root);
 
 // Rotate
-void	ra(t_root *root, bool flag);
-void	rb(t_root *root, bool flag);
-void	rr(t_root *root);
+void		ra(t_root *root, bool flag);
+void		rb(t_root *root, bool flag);
+void		rr(t_root *root);
 
 // Reverse
-void	rra(t_root *root, bool flag);
-void	rrb(t_root *root, bool flag);
-void	rrr(t_root *root);
+void		rra(t_root *root, bool flag);
+void		rrb(t_root *root, bool flag);
+void		rrr(t_root *root);
 
 // Push
-void	pa(t_root *root);
-void	pb(t_root *root);
+void		pa(t_root *root);
+void		pb(t_root *root);
 
 #endif
